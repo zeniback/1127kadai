c88c4653048de731fa517d4ad26f66777ede7118
zeni <zeni0102@gmail.com>
2018-11-26 15:22:15 +0900

    create class

--- 1127kadai.cpp
+++ 1127kadai.cpp
@@ -1,3 +1,26 @@
 #include <stdio.h>
 
- 
+class calculator{
+    public:
+
+    int Add(int x,int y){
+        return x+y;
+    }
+
+    int Sub(int x,int y){
+        return x-y;
+    }
+
+    int Mul(int x,int y){
+        return x*y;
+    }
+
+    int Div(int x,int y){
+        return x/y;
+    }
+};
+
+int main(){
+
+    return 0;
+ }
